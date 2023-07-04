57272b75c7838e70b07efaee870a5e2c12afaa59
AmyNotABot <amelie.simonitsch@gmail.com>
2023-07-04 12:17:58 +0200

    weitere ordner commited

---
+++ BACKEND/src/main/java/com/example/royaltea/RoyalteaApplication.java
@@ -0,0 +1,13 @@
+package com.example.royaltea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalteaApplication.class, args);
+	}
+
+}
---
+++ BACKEND/src/test/java/com/example/royaltea/RoyalteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.royaltea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
