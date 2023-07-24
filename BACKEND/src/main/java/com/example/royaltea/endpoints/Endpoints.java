0a5cf3d154e2f4aa9571a73593abe0172e51356e
AmyNotABot <amelie.simonitsch@gmail.com>
2023-07-24 11:51:58 +0200

    refined adding and receiving bookings from database

--- BACKEND/src/main/java/com/example/royaltea/endpoints/Endpoints.java
+++ BACKEND/src/main/java/com/example/royaltea/endpoints/Endpoints.java
@@ -1,23 +1,37 @@
 package com.example.royaltea.endpoints;
 
 import com.example.royaltea.data.Booking;
+import com.example.royaltea.data.BookingRepository;
 import com.example.royaltea.mail.EmailServiceImpl;
 import com.example.royaltea.mail.MailSenderConfig;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-public class MailEndPoint {
+
+public class Endpoints {
+    final private BookingRepository bookingRepository;
+    public Endpoints(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+    MailSenderConfig mailConfDing = new MailSenderConfig();
     @PostMapping("mail")
     public void sendMail(@RequestBody String to, String name, int amountOfSeats){
+        //---Implementations@
         Booking bookingAdd = new Booking(to, name, amountOfSeats);
-        System.out.println("ICH TRIGGERE");
-
-        MailSenderConfig mailConfDing = new MailSenderConfig();
         EmailServiceImpl mailDing = new EmailServiceImpl(mailConfDing.getJavaMailSender());
-
+        //---Database adding stuff
+        bookingRepository.save(bookingAdd);
+        //---Mail-Stuff
         mailDing.sendSimpleMessage(bookingAdd);
     }
-
+    @GetMapping("milchkaffee")
+        public List<Booking> allBookings(){
+            return bookingRepository.findAll();
+        }
 }
+
+
